ca1c4bebcb55bd08dfc54dc7f4392d8486e47939
cyranawm <cyran.aouameur@gmail.com>
2018-04-16 17:16:10 +0200

    le bon refactor qu'on aime

--- VAE/logVar_VAE.py
+++ VAE/logVar_VAE.py
@@ -10,7 +10,7 @@ Created on Mon Mar 26 18:22:59 2018
 
 import numpy as np
 import torch
-import torch.onnx
+#import torch.onnx
 from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
@@ -97,13 +97,13 @@ class Vanilla_VAE(nn.Module):
         
         #z_sigma = z_var.sqrt()+1e-8
         
-        recon= x_recon_logvar.add(np.log(2 * np.pi))+  (x-x_recon_mu).pow(2).div(torch.exp(x_recon_logvar) + 1e-8)
+        recon= x_recon_logvar.add(np.log(2.0 * np.pi))+  (x-x_recon_mu).pow(2).div(torch.exp(x_recon_logvar) + 1e-8)
         recon = 0.5 * torch.sum(recon,1)
         recon = torch.mean(recon)
         #recon /= (self.mb_size * self.x_dim)
     
         kl_loss = torch.exp(z_logvar) + z_mu**2 - 1. - z_logvar
-        kl_loss = 0.5 * torch.sum(torch.sum(kl_loss,1)) #no size average
+        kl_loss = 0.5 * torch.sum(kl_loss,1) #no size average
         kl_loss = torch.mean(kl_loss)
         
         #loss = recon + kl_loss
@@ -124,10 +124,7 @@ class Vanilla_VAE(nn.Module):
         self.train()
         
         optimizer = optim.Adam(self.parameters(), lr=0.0001)
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=200, gamma=0.5)
-        
-#        dummy_input = Variable(torch.rand(13, 1, 28, 28))
-#        self.writer.add_graph(self, dummy_input)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.7)
         
         epoch_size = 600
         
@@ -147,8 +144,6 @@ class Vanilla_VAE(nn.Module):
                 
                 #iter = epoch*600 + i
                 
-                running_loss, running_recon, running_kl = 0.0, 0.0, 0.0
-                
                 # get the inputs
                 raw_inputs, labels = data
                 
@@ -156,6 +151,7 @@ class Vanilla_VAE(nn.Module):
         
                 # wrap them in Variable
                 inputs, labels = Variable(inputs), Variable(labels)
+                inputs = (inputs*2)-1 #normal rescale
         
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -177,55 +173,40 @@ class Vanilla_VAE(nn.Module):
                         #TENSORBOARD VISUALIZATION
                         for name, param in self.named_parameters():
                             self.writer.add_histogram(name, param.clone().cpu().data.numpy(), epoch+1)
-                            
-                        self.writer.add_scalars('avglosses', {'loss': loss.data[0],
-                                                           'Recon_loss': recon_loss.data[0],
-                                                           'KL_loss': kl_loss.data[0]}, epoch+1)
-                        if np.mod(epoch,50) == 0:
-                            for j in range(2):
-                                original = inputs[j]
-                                original = original.view(28,28)
-                                self.writer.add_image('Original', original, epoch)
-                                
-                                img_rec = x_recon_mu[j]
-                                img_rec = img_rec.view(28,28)
-                                self.writer.add_image('Reconstructed', img_rec, epoch)
-                
-                #Annealing
-                
-    
-                # BACKPROP
-                loss[0].backward()
-                optimizer.step()
-                
-                # print statistics                
-                running_loss += loss.data[0]
-                running_recon += recon_loss.data[0]
-                running_kl += kl_loss.data[0]
-                
-                print('[%d, %5d] \n loss: %.3f \n recon_loss: %.3f \n KLloss: %.3f \n -----------------' %
-                          (epoch + 1, 
-                           i + 1, 
-                           running_loss, 
-                           running_recon, 
-                           running_kl ))
-                
-                #tensorboard plot
-                if self.use_tensorboard:
-                    epoch_loss += loss.data[0]
-                    epoch_recon += recon_loss.data[0]
-                    epoch_KL += kl_loss.data[0]
-                    
-                    if i == epoch_size-1 :
-                        for name, param in self.named_parameters():
-                            self.writer.add_histogram(name, param.clone().cpu().data.numpy(), epoch+1)
+                        
+                        epoch_loss += loss.data[0]
+                        epoch_recon += recon_loss.data[0]
+                        epoch_KL += kl_loss.data[0]
                             
                         self.writer.add_scalars('avglosses', {'loss': epoch_loss/epoch_size,
                                                            'Recon_loss': epoch_recon/epoch_size,
                                                            'KL_loss': epoch_KL/epoch_size},
                                                             epoch+1)
-             
+                        if np.mod(epoch,50) == 0:
+                            for j in range(2):
+                                original = inputs[j]
+                                original = original.view(28,28)
+                                self.writer.add_image('Original_'+str(epoch)+str(j), original, epoch)
+                                
+                                img_rec = x_recon_mu[j]
+                                img_rec = img_rec.view(28,28)
+                                self.writer.add_image('Reconstructed_'+str(epoch)+str(j), img_rec, epoch)
+                
+                #Annealing
+                
+    
+                # BACKPROP
+                loss.backward()
+                optimizer.step()
+                
+            #end of epoch 
             scheduler.step()
+            print('[End of epoch %d] \n beta : %.3f \n loss: %.3f \n recon_loss: %.3f \n KLloss: %.3f \n -----------------' %
+                          (epoch + 1,
+                           beta,
+                           epoch_loss, 
+                           epoch_recon, 
+                           epoch_KL ))
                 
         if self.use_tensorboard:
             self.writer.close()
