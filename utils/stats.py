9607c5185f1e78d8b433a20b3c713ab02d7d43c5
cyranawm <cyran.aouameur@gmail.com>
2018-04-24 15:18:12 +0200

    stats

--- utils/stats.py
+++ utils/stats.py
@@ -12,23 +12,28 @@ import numpy as np
 
 def sound_length(path):
 
+    name_dur = []
     durations = []
     
     for entry in os.scandir(path):
         if entry.is_file():
             y,sr = librosa.load(entry.path)
+            name = entry.path.split('/')[-1]
             dur = librosa.get_duration(y=y, sr=sr)
             durations.append(dur)
+            name_dur.append((name,dur))
             
-    return durations
+    return durations, name_dur
     
    
+def print_stats(path = '/fast_1/DrumsDataset/data/Kicks'): 
+
+    dur, names_dur = sound_length(path)
     
-path = '/fast_1/DrumsDataset/data/Kicks'
-dur = sound_length(path)
-
-mean = np.mean(dur)
-std = np.std(dur)
-max = np.max(dur)
-
-print("mean = %s s \n std = %s s \n max = %s s", (mean, std, max))
+    mean = np.mean(dur)
+    std = np.std(dur)
+    max = np.max(dur)
+    
+    
+    print("mean = %s s \n std = %s s \n max = %s s" % (mean, std, max))
+    return None
