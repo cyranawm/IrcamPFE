035f850b22e0c423fcba40754dc6da544476e215
cyranawm <cyran.aouameur@gmail.com>
2018-05-15 13:01:19 +0200

    new config 3

--- models/VAE/Conv_VAE.py
+++ models/VAE/Conv_VAE.py
@@ -62,17 +62,18 @@ def layers_config(n):
         
     elif n == 3:
         #The conv Layers: [in_channels, out_channels, kernel_size, stride, padding]
-        conv1 = [1, 8, (20,10), (10,5), (2,2)]
-        conv2 = [8, 16, (10,5), (4,4), (0,2)]
+        conv1 = [1, 16, (15,10), (5,3), (0,0)]
+        conv2 = [16, 32, (15,10), (5,3), (0,0)]
         conv = [conv1, conv2]
         
         #The MLP hidden Layers : [[in_dim,hlayer1_dim], [hlayer1_dim,hlayer2_dim], ...] 
-        h_dims = [[2016, 512]]
+        h_dims = [[5376, 1024], [1024, 512]]
         
         #The Deconv Layers: [in_channels, out_channels, kernel_size, stride, padding, output_padding]
-        deconv1 = [16, 8, (10,5), (4,4), (0,2), (1,1)]
-        deconv2 = [8, 1, (13,9), (10,5), (0,2), (0,0)]
-        deconv = [deconv1, deconv2]
+        deconv1 = [32, 16, (15,10), (3,3), (0,0), (0,0)]
+        deconv2 = [16, 8, (20,10), (3,3), (0,0), (0,0)]
+        deconv3 = [8, 1, (5,5), (2,1), (0,0), (0,0)]
+        deconv = [deconv1, deconv2, deconv3]
         
     elif n == 4:
         #The conv Layers: [in_channels, out_channels, kernel_size, stride, padding]
