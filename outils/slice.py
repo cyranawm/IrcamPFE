2151dd8c577300f41758337ccdc2d536b27bc2a3
cyranawm <cyran.aouameur@gmail.com>
2018-05-18 15:44:20 +0200

    slicing and splitting

--- outils/slice.py
+++ outils/slice.py
@@ -8,7 +8,63 @@ Created on Wed May 16 17:50:56 2018
 
 import os
 import librosa
+from numpy.random import permutation
+import numpy as np
 
+def slice_and_split(dataDirectory, trainDirectory, testDirectory, sliceDur, trainRatio = 0.8, overlap = None):
+    files = [i for i in os.listdir(dataDirectory) if i.endswith('.wav')]
+    
+    files = np.permutation(files)
+    train = files[:int(trainRatio*len(files))]
+    test = files[int(trainRatio*len(files)):]
+    
+    
+    for file in train:
+        y,sr = librosa.load(os.path.join(dataDirectory,file))
+        name = file.split('.')[0]
+        fileLen = len(y)
+        sliceLen = int(sliceDur*sr)
+        nbSlices = int(fileLen / sliceLen)
+        
+        if overlap is not None:
+            i=0
+            stepSize = (float(100 - overlap)/100) * sliceLen
+            while (i+1)*stepSize <= fileLen :
+                start = i*stepSize
+                curSlice = y[start : start + sliceLen]
+                path = os.path.join(trainDirectory, name + '_slice' + str(i) + '.wav')
+                librosa.output.write_wav(path, curSlice, sr, norm = True)
+                i += 1
+           
+        else: 
+            for i in range(nbSlices):
+                curSlice = y[i*sliceLen : (i+1)*sliceLen]
+                path = os.path.join(trainDirectory, name + '_slice' + str(i) + '.wav')
+                librosa.output.write_wav(path, curSlice, sr, norm = True)
+                
+                
+    for file in test:
+        y,sr = librosa.load(os.path.join(dataDirectory,file))
+        name = file.split('.')[0]
+        fileLen = len(y)
+        sliceLen = int(sliceDur*sr)
+        nbSlices = int(fileLen / sliceLen)
+        
+        if overlap is not None:
+            i=0
+            stepSize = (float(100 - overlap)/100) * sliceLen
+            while (i+1)*stepSize <= fileLen :
+                start = i*stepSize
+                curSlice = y[start : start + sliceLen]
+                path = os.path.join(testDirectory, name + '_slice' + str(i) + '.wav')
+                librosa.output.write_wav(path, curSlice, sr, norm = True)
+                i += 1
+           
+        else: 
+            for i in range(nbSlices):
+                curSlice = y[i*sliceLen : (i+1)*sliceLen]
+                path = os.path.join(testDirectory, name + '_slice' + str(i) + '.wav')
+                librosa.output.write_wav(path, curSlice, sr, norm = True)
 
 def slice_data(dataDirectory, targetDirectory, sliceDur, overlap = None):
     
@@ -27,14 +83,14 @@ def slice_data(dataDirectory, targetDirectory, sliceDur, overlap = None):
                 while (i+1)*stepSize <= fileLen :
                     start = i*stepSize
                     curSlice = y[start : start + sliceLen]
-                    path = os.path.join(targetDirectory, name + '_slice' + str(i+1) + '.wav')
+                    path = os.path.join(targetDirectory, name + '_slice' + str(i) + '.wav')
                     librosa.output.write_wav(path, curSlice, sr, norm = True)
                     i += 1
                
             else: 
                 for i in range(nbSlices):
                     curSlice = y[i*sliceLen : (i+1)*sliceLen]
-                    path = os.path.join(targetDirectory, name + '_slice' + str(i+1) + '.wav')
+                    path = os.path.join(targetDirectory, name + '_slice' + str(i) + '.wav')
                     librosa.output.write_wav(path, curSlice, sr, norm = True)
                 
 
@@ -53,7 +109,7 @@ if __name__ == '__main__':
                         help='Destination')
     
     parser.add_argument('sliceDur', type=float,
-                        help='<Required> Dimension of the latent space')
+                        help= 'Duration of a slice (in seconds)')
     
     args = parser.parse_args()
     
